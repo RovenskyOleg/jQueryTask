a41408d44e4a97e68c55bf02d9e79884e77250e0
Oleg Rovenskyi <olegrovenskyi@MacBook-Pro-Oleg.local>
2016-05-26 12:00:55 +0300

    Update structure

--- public/js/cartModule.js
+++ public/js/cartModule.js
@@ -1,67 +1,8 @@
-// Сервис который отвечает за получение данных по продуктам и базовой их валидации
+/**
+ * Created by olegrovenskyi on 26.05.16.
+ */
 
-var productService = (function () {
-    var errorMap = {
-            qty: {
-                msg: 'Please set correct product QTY'
-            },
-            maxVal: function (val) {
-                return {
-                    msg: 'Sorry, the maximum quantity you can order is ' + val
-                }
-            }
-        };
-
-    function _getProducts (url) {
-        return $.get(url)
-            .done(_successGetProducts)
-            .fail(_failGetProducts)
-    }
-
-    function _sumTotalPrice (qty, price) {
-        return qty * price;
-    }
-
-    function _validate (value) {
-        var totalProduct = value.totalProduct.stock + value.totalProduct.sklad;
-
-        if (value.pQuantity === 0) {
-            return value.error = errorMap.qty;
-        } else if (value.pQuantity > 99) {
-            return value.error = errorMap.maxVal(99);
-        } else if (value.pQuantity > totalProduct) {
-            return value.error = errorMap.maxVal(totalProduct);
-        } else if (value.isInStoreOnly && value.pQuantity > value.totalProduct.stock) {
-            return value.error = errorMap.maxVal(value.totalProduct.stock);
-        }
-
-        return value;
-    }
-
-    function _successGetProducts (data) {
-        // в данной фугкции можно модифицировать полученный от сервера обьект с данными, если нужно
-        var products = data.products;
-
-        $.each(products, function(key, value) {
-            _validate(value);
-
-            value.totalPrice = _sumTotalPrice(value.pQuantity, value.pPrice);
-        });
-    }
-
-    function _failGetProducts (error) {
-        // если нужно можем расширить обьект с ошибкой
-        console.log('*** _failGetProducts ***', error)
-    }
-
-    function loadData (api) {
-        return _getProducts(api);
-    }
-    
-    return {
-        loadData: loadData
-    }
-}());
+// Модуль для работы с продуктами
 
 var cartModule = (function () {
     var productData = {},
@@ -265,5 +206,3 @@ var cartModule = (function () {
         init: init
     }
 }());
-
-cartModule.init();
---
+++ public/js/init.js
@@ -0,0 +1,4 @@
+/**
+ * Created by olegrovenskyi on 26.05.16.
+ */
+cartModule.init();
